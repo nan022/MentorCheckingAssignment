c46d806b98dcbbeff67d32a64460edbe70531bc6
rezky1710 <rezky20ti@mahasiswa.pcr.ac.id>
2023-06-19 15:06:17 +0700

    Menambahkan fitur JWT

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
+from datetime import datetime,timedelta
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import pandas as pd
@@ -11,7 +12,8 @@ client = MongoClient('mongodb+srv://nande:nande@cluster0.8kafc33.mongodb.net/?re
 db = client.dbmentorcek
 
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True  
+TOKEN_KEY = 'gungnir'
 SECRET_KEY = "MENTORNANDA"
 
 def get_mentor_by_institute(institute_name, course_title):
@@ -21,71 +23,147 @@ def get_mentor_by_institute(institute_name, course_title):
     return mentor_string
 
 @app.route('/', methods=['GET','POST'])
-def sign_in():
-    return render_template("login.html")
+def home():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode (
+            token_receive, 
+            SECRET_KEY, 
+            algorithms=['HS256']
+        )
+        # user_info = db.users.find_one({'username': payload.get('email')})
+        # return render_template("index.html", user_info=user_info)
+        return render_template("home.html")
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+        return redirect(url_for('login', msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was problem logging you in'
+        return redirect(url_for('login', msg=msg))
+    
 
-@app.route('/sign_in_cek', methods=["POST"])
+@app.route('/sign_in', methods=["POST"])
 def sign_in_cek():
     email_receive = request.form['email_give']
     password_receive = request.form['password_give']
-    
-    if email_receive == 'leadmentor@gmail.com' and password_receive == '123456':
-        return jsonify({'result': 'success'})
-    elif email_receive == '' or password_receive == '':
-        print("data kosong")
-        return jsonify({'msg': 'Inputan Tidak Boleh Kosong!'})
+    result = db.users.find_one({
+        "email": email_receive,
+        "password": password_receive,
+    })
+    print(result)
+    if result:
+        payload = {
+            "email": email_receive,
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 2),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return jsonify({
+            "result": "success",
+            "token": token,
+        })
     else:
-        print("Email salah")
-        return jsonify({'msg': 'Email atau Password Salah!'})
+        return jsonify({
+            "result": "fail",
+            "msg": "We could not find a user with that id/password combination",
+        })
 
-@app.route('/home')
-def home():
-    return render_template("home.html")
+@app.route('/login')
+def login():
+    token_receive = request.cookies.get(TOKEN_KEY)
+    return render_template("login.html",token=token_receive)
 
 @app.route('/data_kelas')
 def data_kelas():
-    pipeline = [
-        {
-            '$group': {
-                '_id': {
-                    'mentor': '$mentor',
-                    'program': '$program'
-                },
-                'sekolah': {'$addToSet': '$sekolah'}
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode (
+            token_receive, 
+            SECRET_KEY, 
+            algorithms=['HS256']
+        )
+        # user_info = db.users.find_one({'username': payload.get('email')})
+        # return render_template("index.html", user_info=user_info)
+        pipeline = [
+            {
+                '$group': {
+                    '_id': {
+                        'mentor': '$mentor',
+                        'program': '$program'
+                    },
+                    'sekolah': {'$addToSet': '$sekolah'}
+                }
+            },
+            {
+                '$sort': {
+                    '_id.mentor': 1,
+                    '_id.program': 1
+                }
             }
-        },
-        {
-            '$sort': {
-                '_id.mentor': 1,
-                '_id.program': 1
-            }
-        }
-    ]
-    data_institute = db.institute.find()
-    mentor_list = list(db.kelas.aggregate(pipeline))
-    return render_template("data_mentor.html", mentor_list=mentor_list, data_institute=data_institute)
+        ]
+        data_institute = db.institute.find()
+        mentor_list = list(db.kelas.aggregate(pipeline))
+        return render_template("data_mentor.html", mentor_list=mentor_list, data_institute=data_institute)
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+        return redirect(url_for('login', msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was problem logging you in'
+        return redirect(url_for('login', msg=msg))
+
 
 @app.route('/data_history')
 def data_history():
-    # Mengambil data
-    pipeline = [
-        {
-            '$group': {
-                '_id': '$waktu',
-                'mentors': {
-                    '$push': '$mentor_list'
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode (
+            token_receive, 
+            SECRET_KEY, 
+            algorithms=['HS256']
+        )
+        # user_info = db.users.find_one({'username': payload.get('email')})
+        # return render_template("index.html", user_info=user_info)
+        # Mengambil data
+        pipeline = [
+            {
+                '$group': {
+                    '_id': '$waktu',
+                    'mentors': {
+                        '$push': '$mentor_list'
+                    }
                 }
             }
-        }
-    ]
+        ]
+        data = db.record_mentor.aggregate(pipeline)
+        return render_template("mentor_history.html", data_list=data)
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+        return redirect(url_for('login', msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was problem logging you in'
+        return redirect(url_for('login', msg=msg))
     
-    data = db.record_mentor.aggregate(pipeline)
-    return render_template("mentor_history.html", data_list=data)
+    
 
 @app.route('/institute_course')
 def institute_course():
-    data = db.institute.find()
-    return render_template("institute.html", data=data)
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode (
+            token_receive, 
+            SECRET_KEY, 
+            algorithms=['HS256']
+        )
+        # user_info = db.users.find_one({'username': payload.get('email')})
+        # return render_template("index.html", user_info=user_info)     
+        data = db.institute.find()
+        return render_template("institute.html", data=data)
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+        return redirect(url_for('login', msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was problem logging you in'
+        return redirect(url_for('login', msg=msg))
+    
 
 @app.route('/delete_data', methods=['POST'])
 def delete_data():
@@ -142,50 +220,64 @@ def checking_post():
 
 @app.route('/mentor_check', methods=['GET', 'POST'])
 def mentor_check():
-    if request.method == 'POST':
-        file = request.files['file']
-        time_off = datetime.datetime.strptime(request.form['time_off'], '%Y-%m-%dT%H:%M')
+    token_receive = request.cookies.get(TOKEN_KEY)
+    try:
+        payload = jwt.decode (
+            token_receive, 
+            SECRET_KEY, 
+            algorithms=['HS256']
+        )
+        # user_info = db.users.find_one({'username': payload.get('email')})
+        # return render_template("index.html", user_info=user_info)
+        if request.method == 'POST':
+            file = request.files['file']
+            time_off = datetime.datetime.strptime(request.form['time_off'], '%Y-%m-%dT%H:%M')
 
-        if file:
-            df = pd.read_csv(file)
-            selected_columns = ['Learners Name', 'Institute Name', 'Course Title', 'Submission Date', 'Status']
-            selected_data = df[selected_columns]
+            if file:
+                df = pd.read_csv(file)
+                selected_columns = ['Learners Name', 'Institute Name', 'Course Title', 'Submission Date', 'Status']
+                selected_data = df[selected_columns]
 
-            current_date = datetime.datetime.now().date()
-            previous_day = current_date - datetime.timedelta(days=1)
-            cutoff_time = datetime.datetime.combine(previous_day, datetime.time(hour=21, minute=0))
+                current_date = datetime.datetime.now().date()
+                previous_day = current_date - datetime.timedelta(days=1)
+                cutoff_time = datetime.datetime.combine(previous_day, datetime.time(hour=21, minute=0))
 
-            grouped_data = defaultdict(list)
-            for index, row in selected_data.iterrows():
-                date_string = row['Submission Date']
-                date_value = float(date_string)
-                date_format = pd.to_datetime(date_value, unit='ms')
+                grouped_data = defaultdict(list)
+                for index, row in selected_data.iterrows():
+                    date_string = row['Submission Date']
+                    date_value = float(date_string)
+                    date_format = pd.to_datetime(date_value, unit='ms')
 
-                if row['Status'] == 'pending' and date_format < time_off:
-                    institute_name = row['Institute Name']
-                    course_title = row['Course Title']
+                    if row['Status'] == 'pending' and date_format < time_off:
+                        institute_name = row['Institute Name']
+                        course_title = row['Course Title']
+                        item = {
+                            'Learners Name': row['Learners Name'],
+                            'Institute Name': institute_name,
+                            'Course Title': course_title,
+                            'SubmissionDate': date_format.strftime('%Y-%m-%d %H:%M'),
+                            'Status': row['Status'],
+                            'MentorList': get_mentor_by_institute(institute_name, course_title)
+                        }
+                        grouped_data[(institute_name, course_title)].append(item)
+
+                result = []
+                for key, data_list in grouped_data.items():
                     item = {
-                        'Learners Name': row['Learners Name'],
-                        'Institute Name': institute_name,
-                        'Course Title': course_title,
-                        'SubmissionDate': date_format.strftime('%Y-%m-%d %H:%M'),
-                        'Status': row['Status'],
-                        'MentorList': get_mentor_by_institute(institute_name, course_title)
+                        'Institute Name': key[0],
+                        'Course Title': key[1],
+                        'DataList': data_list
                     }
-                    grouped_data[(institute_name, course_title)].append(item)
+                    result.append(item)
 
-            result = []
-            for key, data_list in grouped_data.items():
-                item = {
-                    'Institute Name': key[0],
-                    'Course Title': key[1],
-                    'DataList': data_list
-                }
-                result.append(item)
-
-            return render_template('checking_mentor.html', result=result)
-
-    return render_template('checking_mentor.html')
-
+                return render_template('checking_mentor.html', result=result)
+        return render_template('checking_mentor.html')
+        
+    except jwt.ExpiredSignatureError:
+        msg = 'Your token has expired'
+        return redirect(url_for('login', msg=msg))
+    except jwt.exceptions.DecodeError:
+        msg = 'There was problem logging you in'
+        return redirect(url_for('login', msg=msg))
 if __name__ == '__main__':
     app.run(debug=True)
